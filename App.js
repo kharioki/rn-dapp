ca68ad39ad1fe61b2906c2f62315e75ee8f65653
tony <antolisah@gmail.com>
2022-02-15 22:31:31 +0300

    setup walletconnect config

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import WalletConnectProvider from '@walletconnect/react-native-dapp';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WalletConnectProvider
+      bridge="https://bridge.walletconnect.org"
+      clientMeta={{
+        description: 'Connect with WalletConnect',
+        url: 'https://walletconnect.org',
+        icons: ['https://walletconnect.org/walletconnect-logo.png'],
+        name: 'WalletConnect',
+      }}
+      redirectUrl={Platform.OS === 'web' ? window.location.origin : 'yourappscheme://'}
+      storageOptions={{
+        asyncStorage: AsyncStorage,
+      }}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </WalletConnectProvider>
   );
 }
 
